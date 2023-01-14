bdcc7b81c8d5bc30a1a616199c042037c936026c
Aleksandr Matveev <aleksandrmatveev99@yandex.ru>
2023-01-14 16:06:32 +0300

    Add Seminar_1

---
+++ Seminar_1/Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+// Запрос чисел у пользователя
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+// Развилка
+if(number1 == number2 * number2)
+{
+    Console.WriteLine($"Первое число {number1} является квадратом второго числа {number2}");
+}
+else
+{
+    Console.WriteLine($"Первое число {number1} не является квадратом второго числа {number2}");
+}
---
+++ Seminar_1/Task_3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+// Запрос числа от пользователя
+Console.Write("Введите порядковый номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// Условия
+if(number == 1)
+{
+    Console.WriteLine($"{number} день недели - понедельник");
+}
+else if(number == 2)
+{
+    Console.WriteLine($"{number} день недели - вторник");
+}
+else if(number == 3)
+{
+    Console.WriteLine($"{number} день недели - среда");
+}
+else if(number == 4)
+{
+    Console.WriteLine($"{number} день недели - четверг");
+}
+else if(number == 5)
+{
+    Console.WriteLine($"{number} день недели - пятница");
+}
+else if(number == 6)
+{
+    Console.WriteLine($"{number} день недели - суббота");
+}
+else if(number == 7)
+{
+    Console.WriteLine($"{number} день недели - воскресенье");
+}
+else
+{
+    Console.WriteLine($"Такого дня недели не существует");
+}
