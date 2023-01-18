cb2c17e02743513dede9825928dca852e2231f32
Aleksandr Matveev <aleksandrmatveev99@yandex.ru>
2023-01-19 00:01:48 +0300

    Add Task_21

---
+++ Seminar_3/Task_21/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+Console.Write("Введите координату x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату у1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату у2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+Console.WriteLine($"Расстояние между точками: {Math.Round(result, 2)}");
