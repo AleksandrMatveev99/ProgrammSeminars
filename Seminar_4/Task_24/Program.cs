4a5d11767deb8b85284b5cfa0727ef2955b483e6
Aleksandr Matveev <aleksandrmatveev99@yandex.ru>
2023-01-20 21:39:29 +0300

    Add Task_24

---
+++ Seminar_4/Task_24/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetSumToA(int a)
+{
+    int sum = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        sum+=i;
+    }
+    return sum;
+}
+
+int number = ReadNumber("Введите число: ");
+if (number > 0)
+{
+    int summa = GetSumToA(number);
+    Console.WriteLine($"Сумма = {summa}");
+}
+else
+{
+    Console.WriteLine("Введено неверное число");
+}
