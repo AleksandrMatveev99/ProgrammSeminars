1434e93cbfb32de52afa1c72c34a385abb49b769
Aleksandr Matveev <aleksandrmatveev99@yandex.ru>
2023-01-20 22:37:32 +0300

    Add Task_26

---
+++ Seminar_4/Task_26/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int CountDigit(int remainingNumber)
+{
+    int count = 0;
+    while (remainingNumber != 0)
+    {
+        remainingNumber = (remainingNumber - remainingNumber % 10) / 10;
+        count++;
+    }
+    return count;
+}
+// int number = ReadNumber("Введите число: ");
+// int countDig = CountDigit(number);
+// Console.WriteLine(countDig);
+Console.WriteLine(-546 % 10);
