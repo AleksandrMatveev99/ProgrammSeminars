a3c53098d32fea16be4d99e5aa871826aa0394c4
Aleksandr Matveev <aleksandrmatveev99@yandex.ru>
2023-01-20 22:51:12 +0300

    Add Task_28

---
+++ Seminar_4/Task_28/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int MultiplicationNumber(int number)
+{
+    int proizved = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        proizved *= i;
+    }
+    return proizved;
+}
+
+int N = ReadNumber("Введите число: ");
+if (N < 0)
+{
+    Console.WriteLine("Неверное число");
+}
+else
+{
+    int multiplication = MultiplicationNumber(N);
+    Console.WriteLine(multiplication);
+}
