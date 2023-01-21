41781d1f0298c718405cfde6224083fdfa607ce1
Aleksandr Matveev <aleksandrmatveev99@yandex.ru>
2023-01-21 17:45:07 +0300

    Add Task_32

---
+++ Seminar_5/Task_32/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу замена элементов массива: 
+//положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+int[] GetRandomNumber(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+Console.Write("Введите количество элементов массива: ");
+int count = Convert.ToInt32(Console.ReadLine());
+if (count<1) 
+{
+    Console.WriteLine("Введите положительное число");
+    return;
+}
+Console.Write("Введите левую границу: ");
+int range1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите правую границу: ");
+int range2 = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = GetRandomNumber(count, range1, range2);
+Console.WriteLine(string.Join(" ", arr));
+
+for (int index = 0; index < arr.Length; index++)
+{
+    arr[index] *= -1;
+}
+Console.WriteLine(string.Join(" ", arr));
