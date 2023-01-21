5cc66896191e0f014bc1998744d2eea59c8edd96
Aleksandr Matveev <aleksandrmatveev99@yandex.ru>
2023-01-21 17:45:30 +0300

    Add Task_33

---
+++ Seminar_5/Task_33/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+
+int[] GetRandomArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+void FindNumber(int[] massive, int findNumber)
+{
+    bool answer = false;
+    for (int index = 0; index < massive.Length; index++)
+    {
+        if (massive[index] == findNumber)
+        {
+            answer = true;
+            break;
+        }
+    }
+    if (answer == true)
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
+
+Console.Write("Введите длину массива: ");
+int sizeArr = Convert.ToInt32(Console.ReadLine());
+if (sizeArr < 1)
+{
+    Console.WriteLine("Число должно быть положительным");
+    return;
+}
+
+Console.Write("Введите минимальное значение: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите искомое значение: ");
+int findValue = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = GetRandomArray(sizeArr, minValue, maxValue);
+Console.WriteLine(string.Join(" ", arr));
+
+FindNumber(arr, findValue);
