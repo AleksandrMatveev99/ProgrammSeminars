cdcfa39e9c4d8cc526595a8bf896cded914b4da0
Aleksandr Matveev <aleksandrmatveev99@yandex.ru>
2023-02-12 16:27:54 +0300

    Add Task_51

---
+++ Seminar_7/Task_51/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+// ​
+// Доп. условие для 51 задачи: сделать суммирование в один цикл. Матрица может быть прямоугольной
+
+int[,] GetRandomMatrix(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumNumber(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0) && i < matrix.GetLength(1); i++)
+    {
+        sum += matrix[i, i];
+    }
+    return sum;
+}
+
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимальное значение: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное значение: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = GetRandomMatrix(m, n, minValue, maxValue);
+PrintMatrix(array);
+Console.WriteLine();
+
+int sumMain = SumNumber(array);
+Console.WriteLine(sumMain);
