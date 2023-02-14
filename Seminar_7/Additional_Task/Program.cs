a078fbf652aadbde0eae2544e6a8c5c93987313e
Aleksandr Matveev <aleksandrmatveev99@yandex.ru>
2023-02-14 10:57:04 +0300

    Закомментировал печать

--- Seminar_7/Additional_Task/Program.cs
+++ Seminar_7/Additional_Task/Program.cs
@@ -23,6 +23,10 @@ void PrintMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
+        // for (int j = matrix.GetLength(0); j > i; j--)
+        // {
+        //     Console.Write(" ");
+        // }
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
             if (matrix[i, j] == 0) break;
