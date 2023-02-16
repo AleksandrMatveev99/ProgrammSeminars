209667b0b0e8a88adf9058170f7e4fdffea89a63
Aleksandr Matveev <aleksandrmatveev99@yandex.ru>
2023-02-16 21:11:07 +0300

    Add Task_53

---
+++ Seminar_8/Task_53/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задайте двумерный массив. Напишите программу, 
+// которая поменяет местами первую и последнюю строку массива.
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetRandomMatrix(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReducingLines(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int temp = matrix[0,j];
+        matrix[0,j] = matrix[matrix.GetLength(0)-1, j];
+        matrix[matrix.GetLength(0)-1, j] = temp;
+    }
+}
+
+int m = ReadNumber("Введите количество строк: ");
+int n = ReadNumber("Введите количество столбцов: ");
+int maxValue = ReadNumber("Введите максимальное число: ");
+int minValue = ReadNumber("Введите минимальное число: ");
+
+int[,] array = GetRandomMatrix(m, n, maxValue, minValue);
+PrintMatrix(array);
+Console.WriteLine();
+ReducingLines(array);
+PrintMatrix(array);
