a0d65595958cff70d2953ebc68a3632ed40c556d
Aleksandr Matveev <aleksandrmatveev99@yandex.ru>
2023-02-20 18:34:34 +0300

    Add Demonstration Task

---
+++ Seminar_9/Demonstration_Task/Program.cs
@@ -0,0 +1,11 @@
+﻿// Перевести десятичное число в двоичное
+
+void PrintBinaryView(int n)
+{
+    if (n <= 0) return;
+    PrintBinaryView(n / 2);
+    Console.Write(n % 2);
+}
+
+int number = 13;
+PrintBinaryView(number);
