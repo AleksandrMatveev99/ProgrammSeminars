a5f285ae3f0afec992ed1e419fa758237ea2416c
Aleksandr Matveev <aleksandrmatveev99@yandex.ru>
2023-02-20 19:38:14 +0300

    Add Task_67

---
+++ Seminar_9/Task_67/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int SumDigit(int number)
+{
+    if (number == 0) return number;
+    return number % 10 + SumDigit(number / 10);
+}
+
+Console.Write("Введите натуральное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int sum = SumDigit(N);
+Console.Write(sum);
